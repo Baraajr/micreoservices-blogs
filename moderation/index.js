dd9d0450dc9647b132d79a61d998e484b078fe1b
Ahmed baraa <ahmedbaraa009@gmail.com>
2025-06-09 13:30:48 +0300

    added moderation service

---
+++ moderation/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+app.use(express.json());
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === 'CommentCreated') {
+    console.log('Event Received:', type, data);
+    const status = data.content.includes('orange') ? 'rejected' : 'approved';
+    console.log('Comment Moderation Status:', status);
+    const dataToSend = {
+      id: data.id,
+      postId: data.postId,
+      status,
+      content: data.content,
+    };
+    console.log('Data to be sent:', dataToSend);
+    // send event to event bus
+    axios
+      .post('http://localhost:4005/events', {
+        type: 'CommentModerated',
+        data: dataToSend,
+      })
+      .catch((err) => {
+        console.error('Error sending event:', err.message);
+      });
+  }
+  res.send({});
+});
+
+app.listen(4003, () => {
+  console.log('Listening on 4003');
+});
