23cf7b5c82ba237447eee99d70bd7f9dce4cd705
Ahmed baraa <ahmedbaraa009@gmail.com>
2025-06-09 13:29:58 +0300

    added status

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
 const axios = require('axios');
+const { stat } = require('fs');
 
 const app = express();
 app.use(express.json());
@@ -19,16 +20,18 @@ app.post('/posts/:id/comments', async (req, res) => {
 
   const comments = commentsByPostId[req.params.id] || [];
 
-  comments.push({ id: commentId, content });
+  comments.push({ id: commentId, content, status: 'pending' });
 
   commentsByPostId[req.params.id] = comments;
 
+  // send event to event bus
   await axios.post('http://localhost:4005/events', {
     type: 'CommentCreated',
     data: {
       id: commentId,
       content,
       postId: req.params.id,
+      status: 'pending',
     },
   });
 
@@ -36,7 +39,38 @@ app.post('/posts/:id/comments', async (req, res) => {
 });
 
 app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+
   console.log('Event Received', req.body.type);
+
+  if (type === 'CommentModerated') {
+    const { id, postId, status, content } = data;
+
+    const comments = commentsByPostId[postId];
+    console.log('Comments for postId:', postId, comments);
+    const comment = comments.find((comment) => {
+      return comment.id === id;
+    });
+
+    if (comment) {
+      comment.status = status;
+
+      // send event to event bus
+      axios
+        .post('http://localhost:4005/events', {
+          type: 'CommentUpdated',
+          data: {
+            id,
+            postId,
+            status,
+            content,
+          },
+        })
+        .catch((err) => {
+          console.error('Error sending event:', err.message);
+        });
+    }
+  }
 
   res.send({});
 });
